9a860efdb64dbcbe053fe561f42df9f6f85317d5
sgrg007 <sgrg007@gmail.com>
2022-03-20 09:42:32 -0500

    initial commit

---
+++ src/main/java/com/gurung/awskinesisproducer/AwsKinesisProducerApplication.java
@@ -0,0 +1,13 @@
+package com.gurung.awskinesisproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsKinesisProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsKinesisProducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gurung/awskinesisproducer/AwsKinesisProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gurung.awskinesisproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsKinesisProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
